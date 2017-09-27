dd54eaa508eae37411d6c4178fcef878fe7622be
Konsta Purtsi <kovipu@utu.fi>
2017-09-27 12:44:52 +0300

    improved demo2 t5

--- Demo2/t5.asm
+++ Demo2/t5.asm
@@ -1,6 +1,10 @@
+	.data
+data:	.word 1,2,3,4,5,6,7
+	.text
+	
 	# the words are in memory addresses
 	# 0x10010000 - 0x1001001b
-	li $a0,0x10010000
+	la $a0,data
 	# stack is in memory addresses
 	# 0x1001001c - 0x10010038
 	li $sp,0x10010038
@@ -13,7 +17,7 @@ push:	subi $sp,$sp,4
 	subi $t0,$t0,1
 	bne $t0,$zero,loop
 	
-	li $a0,0x10010000
+	la $a0,data
 	li $t0,7
 loop2:	lw $t1,0($sp)
 	addi $sp,$sp,4
