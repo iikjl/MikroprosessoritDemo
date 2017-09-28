698b39101fa94b35bd641798b90ddbbd233c93dd
Konsta Purtsi <kovipu@utu.fi>
2017-09-28 12:26:01 +0300

    solved demo3 t1

---
+++ Demo3/t1.asm
@@ -0,0 +1,5 @@
+	blt $s0, $s1, L1
+L1: 	nop
+# Blt on pseudokäsky jonka kääntäjä muuttaa oikeiksi käskyiksi.
+# Muita haarautumis pseudokäskyjä ovat bge, bgt ja ble.
+# https://en.wikibooks.org/wiki/MIPS_Assembly/Pseudoinstructions
