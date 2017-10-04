3a5f304dc3467e45e0bfbb0c5bac72b3932af67a
iikjl <iikka.luoma-aho@outlook.com>
2017-10-04 08:24:43 +0300

    Update t1.asm

--- Demo3/t1.asm
+++ Demo3/t1.asm
@@ -3,3 +3,6 @@ L1: 	nop
 # Blt on pseudokäsky jonka kääntäjä muuttaa oikeiksi käskyiksi.
 # Muita haarautumis pseudokäskyjä ovat bge, bgt ja ble.
 # https://en.wikibooks.org/wiki/MIPS_Assembly/Pseudoinstructions
+
+#kääntyy slt $1, $8, $9
+	#bne $1, $0, label
